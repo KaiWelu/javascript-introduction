63769e085fbe7e452bfc44fb4f6ee9f03b0fc52a
Kai Weluda <kaiweluda@gmail.com>
2023-04-06 14:14:42 +0200

    feat: adds sumOfCharacters function

--- arrays/script.js
+++ arrays/script.js
@@ -5,14 +5,14 @@ function removeItem(array, position) {
   return newArray;
 }
 
-const ainmals = ["Dog", "Cat", "Lion"];
+const animals = ["Dog", "Cat", "Lion"];
 
 console.log("Task 1");
 
-console.log(removeItem(ainmals, 1));
+console.log(removeItem(animals, 1));
 // result should be: ["Cat", "Lion"]
 
-console.log(ainmals);
+console.log(animals);
 // result should be still: ["Dog", "Cat", "Lion"]
 
 const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
@@ -21,3 +21,29 @@ console.log(removeItem(fruits, 3));
 
 console.log(fruits);
 // result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
+
+//Task 2 sumOfCharacters function
+
+function sumOfCharacters(array) {
+  let count = 0;
+  for (i = 0; i < array.length; i++) {
+    if (typeof array[i] === typeof "") {
+      count += array[i].length;
+    }
+  }
+  return count;
+}
+
+const arr1 = ["Luke", "Anakin", true, "Obi Wan", 333];
+console.log(sumOfCharacters(arr1));
+// result should be: 17
+
+const arr2 = [
+  "Code is",
+  "like humor",
+  ".",
+  "When you have",
+  "to explain it, it's bad!",
+];
+console.log(sumOfCharacters(arr2));
+// result should be: 55
