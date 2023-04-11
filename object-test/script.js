bcf62d6fd19f2b32744ab843b546c60c08e563b9
Kai Weluda <kaiweluda@gmail.com>
2023-04-11 15:20:19 +0200

    feat: adds examples for objects and classes

---
+++ object-test/script.js
@@ -0,0 +1,15 @@
+// this is how you create an object
+const testObject = {
+  firstName: "Kai",
+  lastName: "Weluda",
+};
+
+console.log(testObject);
+
+//this is how classes work
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
