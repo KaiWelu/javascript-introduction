b66471470c84bce6b804412f3b57ae6a8c01c08f
Kai Weluda <kaiweluda@gmail.com>
2023-04-12 16:00:45 +0200

    feat: adds counting sheep kata

---
+++ codewars-katas/script.js
@@ -0,0 +1,39 @@
+// Counting Sheep - https://www.codewars.com/kata/54edbc7200b811e956000556
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+var array1 = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+console.log(countSheeps(array1));
