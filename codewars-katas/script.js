5ad9ef11c11b325e1622b5ba6f10e84fb86ef84f
Kai Weluda <kaiweluda@gmail.com>
2023-04-12 16:28:01 +0200

    feat: adds people on bus kata

--- codewars-katas/script.js
+++ codewars-katas/script.js
@@ -27,3 +27,13 @@ function makeNegative(num) {
   }
   return num;
 }
+
+// Number of People in the Bus - https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/javascript
+var number = function (busStops) {
+  busStops = busStops.map((element) => element[0] - element[1]);
+  let people = 0;
+  for (let i = 0; i < busStops.length; i++) {
+    people += busStops[i];
+  }
+  return people;
+};
