bd527f6b926ac360801a3158523a4d3d806cad1d
Kai Weluda <kaiweluda@gmail.com>
2023-04-12 16:08:18 +0200

    feat: adds return positive kata

--- codewars-katas/script.js
+++ codewars-katas/script.js
@@ -19,3 +19,11 @@ function positiveSum(arr) {
   }
   return sum;
 }
+
+// Return Positive - https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  if (num > 0) {
+    num *= -1;
+  }
+  return num;
+}
