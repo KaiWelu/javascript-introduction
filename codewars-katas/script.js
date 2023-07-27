856f5f8808f1aaa525b0b7ca1816910d9c0865e5
Kai Weluda <kaiweluda@gmail.com>
2023-07-27 10:20:50 +0200

    feat: adds multiples of 3 and 5 kata

--- codewars-katas/script.js
+++ codewars-katas/script.js
@@ -338,4 +338,16 @@ function duplicateCount(text) {
   return output;
 }
 
-duplicateCount("Yolo");
+// Multiples of 3 or 5 - https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+function solution(number) {
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum = sum + i;
+    }
+  }
+  return sum;
+}
+
+solution(10);
